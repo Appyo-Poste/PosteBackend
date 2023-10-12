3d60bb472b285573c7d4da056bf687b96c56e9d3
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2023-10-11 20:44:48 -0500

    update create_folder; can_action_folder/post; share methods

--- PosteAPI/models.py
+++ PosteAPI/models.py
@@ -9,18 +9,40 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=150, blank=True)
 
     def create_folder(self, title):
-        return Folder.objects.create(title=title, creator=self)
+        folder = Folder.objects.create(title=title, creator=self)
+        FolderPermission.objects.create(
+            user=self,
+            folder=folder,
+            permission=FolderPermissionEnum.FULL_ACCESS
+        )
+        return folder
 
     def create_post(self, title, url, folder):
         return Post.objects.create(title=title, url=url, creator=self, folder=folder)
 
-    # @TODO update given FolderPermission
-    def can_see_folder(self, folder):
-        return folder.creator == self or self in folder.shared_users.all()
+    def can_view_folder(self, folder):
+        return FolderPermission.objects.filter(
+            user=self, folder=folder, permission__in=[
+                FolderPermissionEnum.FULL_ACCESS, FolderPermissionEnum.EDITOR, FolderPermissionEnum.VIEWER]).exists()
 
-    # @TODO update given FolderPermission
-    def can_see_post(self, post):
-        return self.can_see_folder(post.folder)
+    def can_view_post(self, post):
+        return self.can_view_folder(post.folder)
+
+    def can_edit_folder(self, folder):
+        return FolderPermission.objects.filter(
+            user=self, folder=folder, permission__in=[
+                FolderPermissionEnum.FULL_ACCESS, FolderPermissionEnum.EDITOR]).exists()
+
+    def can_edit_post(self, post):
+        return self.can_edit_folder(post.folder)
+
+    def can_share_folder(self, folder):
+        return FolderPermission.objects.filter(
+            user=self, folder=folder, permission__in=[
+                FolderPermissionEnum.FULL_ACCESS]).exists()
+
+    def can_share_post(self, post):
+        return self.can_share_folder(post.folder)
 
     def create_post_and_folder(self, title, description, url, folder_title):
         folder = Folder.objects.create(title=folder_title, creator=self)
@@ -29,17 +51,25 @@ class User(AbstractUser):
         )
         return post, folder
 
-    # @TODO update given FolderPermission
-    def share_folder_with_user(self, folder, user):
-        if folder.creator != self:
-            raise Exception("You are not the creator of this folder")
-        folder.shared_users.add(user)
+    def share_folder_with_user(self, folder, user, permission):
+        if not self.can_share_folder(folder):
+            raise Exception("You do not have permission to share this folder.")
+        FolderPermission.objects.create(
+            user=user,
+            folder=folder,
+            permission=permission
+        )
 
-    # @TODO update given FolderPermission
     def unshare_folder_with_user(self, folder, user):
-        if folder.creator != self:
-            raise Exception("You are not the creator of this folder")
-        folder.shared_users.remove(user)
+        if not self.can_share_folder(folder):
+            raise Exception("You do not have permission to unshare this folder.")
+        folder_permission = FolderPermission.objects.get(
+            user=user,
+            folder=folder
+        )
+        if not folder_permission:
+            raise Exception("User does not have access to this folder.")
+        folder_permission.delete()
 
     def save(self, *args, **kwargs):
         self.email = self.email.lower()
@@ -69,17 +99,18 @@ class Post(models.Model):
         return self.title
 
 
-class FolderPermission(models.Model):
-    # why gettext_lazy?
-    # https://stackoverflow.com/questions/54802616/how-can-one-use-enums-as-a-choice-field-in-a-django-model
-    class FolderPermissionEnum(models.TextChoices):
-        # a viewer can only view posts within a folder
-        VIEWER = 'viewer', gettext_lazy('Viewer')
-        # an editor can add posts to a folder, edit existing posts, and delete posts
-        EDITOR = 'editor', gettext_lazy('Editor')
-        # a full access user is an editor who can share the folder with other users
-        FULL_ACCESS = 'full_access', gettext_lazy('Full Access')
+# why gettext_lazy?
+# https://stackoverflow.com/questions/54802616/how-can-one-use-enums-as-a-choice-field-in-a-django-model
+class FolderPermissionEnum(models.TextChoices):
+    # a viewer can only view posts within a folder
+    VIEWER = 'viewer', gettext_lazy('Viewer')
+    # an editor can add posts to a folder, edit existing posts, and delete posts
+    EDITOR = 'editor', gettext_lazy('Editor')
+    # a full access user is an editor who can share the folder with other users
+    FULL_ACCESS = 'full_access', gettext_lazy('Full Access')
 
+
+class FolderPermission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
     permission = models.CharField(max_length=12, choices=FolderPermissionEnum.choices)
