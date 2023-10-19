aada9011e2c357db66a5cff0e74c8f42aa0f02e4
David McConnell <mccodav1@gmail.com>
2023-10-19 08:51:22 +0200

    Clarify URLs

--- PosteAPI/urls.py
+++ PosteAPI/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("users/", UsersView.as_view(), name="user-list"),  # GET to list users, POST to create a user
     path("users/<int:pk>/", UserDetail.as_view(), name="user-detail"),
     path("login/", LoginView.as_view(), name="user-login"),  # POST to login
-    path("data/", DataView.as_view(), name="user-folders"),  # Functions as data retrieval / update endpoint
+    path("data/", DataView.as_view(), name="user-folders"),  # GET to retrieve folders and posts to update client
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
