e6f8747c0d69bb6e2ba0c2036a6e127ec7063aaa
David McConnell <mccodav1@gmail.com>
2023-10-27 08:35:42 +0200

    Update admin pages for each model

--- PosteAPI/admin.py
+++ PosteAPI/admin.py
@@ -3,7 +3,32 @@ from django.contrib import admin
 
 from .models import Folder, Post, User, FolderPermission
 
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Folder)
-admin.site.register(FolderPermission)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name')
+    # order by email alphabetically
+    ordering = ['id']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url', 'folder', 'creator')
+    # order by folder alphabetically
+    ordering = ['folder__title']
+
+
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'creator')
+    # order by creator and title alphabetically
+    ordering = ['creator', 'title']
+
+
+class FolderPermissionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'folder', 'permission')
+    # order by folder alphabetically
+    ordering = ('folder__title',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Folder, FolderAdmin)
+admin.site.register(FolderPermission, FolderPermissionAdmin)
