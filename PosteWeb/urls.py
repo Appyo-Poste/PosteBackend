55dfb233e3dd0ff8700fdc6acf050397cdf3d734
Andrew Treichler <atreichl@asu.edu>
2024-01-22 14:40:25 -0600

    Made the first iteration of the landing page.

--- PosteWeb/urls.py
+++ PosteWeb/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, folderPage, checkLogin, login_page
+from .views import index, folderPage, checkLogin, login_page, landing_page, sign_up
 
 urlpatterns = [
-    path("", login_page, name="login"),
+    path("", landing_page, name="landing"),
+    path("login/", login_page, name="login"),
     path("folder/", folderPage.as_view(), name="folders"),
     path("loginCheck/", checkLogin, name="login check"),
+    path("register/", sign_up.as_view(), name="register"),
 ]
--- PosteWeb/views.py
+++ PosteWeb/views.py
@@ -7,6 +7,12 @@ from django.views.generic.list import ListView
 from PosteAPI.models import Folder, User, FolderPermissionEnum
 from PosteWeb.forms import LoginForm
 from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.views import generic
+
+User = get_user_model()
 
 import pprint
 # Create your views here.
@@ -58,7 +64,16 @@ def login_page(request):
         if user is not None:
             login(request, user)
             message = f'Hello {user.username}! You have been logged in'
-            return redirect("folder/")
+            return redirect("folders")
         else:
             message = "login failed"
     return render(request,'Login.html', context={'form': form, 'message': message})
+
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
+class sign_up(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login/")
+    template_name = "sign_up.html"
